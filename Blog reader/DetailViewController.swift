1efd42ee4455c81e8524dfdc5625a3ed58ba8b4e
Angel Caro <angelcaro@angels-macbook-pro.local>
2016-04-04 10:19:14 -0600

    Initial Commit

---
+++ Blog reader/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  Blog reader
+//
+//  Created by Angel Caro on 2/29/16.
+//  Copyright © 2016 AngelCaro. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    
+    @IBOutlet var webView: UIWebView!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let postWebView = self.webView {
+                
+                postWebView.loadHTMLString(detail.valueForKey("content")!.description, baseURL: NSURL(string: "https://googleblog.blogspot.com"))
+              
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
